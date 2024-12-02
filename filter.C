5523a79591345fab2f1c28dc7e7be91c4bcc97da
Pablo Pomares <pablo.pomaresv@alumno.buap.mx>
2024-12-02 06:10:22 -0600

    Avance filtro, adición de btag y global muon

--- filter.C
+++ filter.C
@@ -1,30 +1,62 @@
-void prueba_filtro1(){
+// **ROOT**
+// Version: ROOT 6.32.04
+// Description: Filter for events containing less than 4 muons and 
+//              all jets that passes a btag
+//
+// Created: 30 Nov 2024
+// Last Modified: 01 Dec 2024
+// Author: Pablo Pomares
+// Email: pablo.pomaresv@alumno.buap.mx
+//
+
+void filter(){
   TFile *f_old = TFile::Open("root://eospublic.cern.ch//eos/opendata/cms/Run2016H/DoubleMuon/NANOAOD/UL2016_MiniAODv2_NanoAODv9-v1/2510000/127C2975-1B1C-A046-AABF-62B77E757A86.root");
   TTree* t_old;
   f_old->GetObject("Events", t_old);
 
-  // Número de entradas
   const int nentries = t_old->GetEntries();
 
-  // Desactivo las todas las branches
+  // Deactivate all branches
   t_old->SetBranchStatus("*", 0);
 
-  // Activo aquellas que quiero
-  for (auto actBranchName : {"Muon_charge", "Muon_dxy", "Muon_dxyErr", "Muon_isGlobal", 
+  // Activate desired branches
+  for (auto actBranchName : {"run", "event", "Muon_charge", "Muon_dxy", "Muon_dxyErr", "Muon_isGlobal", 
                              "Muon_isTracker", "Muon_pt", "Muon_phi", "Muon_eta", "nMuon", "MET_phi",
                              "MET_pt", "MET_significance", "nJet", "Jet_btagCSVV2", "Jet_btagDeepB"}){
     t_old->SetBranchStatus(actBranchName, 1);
   };
 
+  // Entry selection
+  UInt_t nMuon, nJet;
+  Float_t Jet_btagDeepB[20];
+  Bool_t Muon_isGlobal[10];
+  t_old->SetBranchAddress("nMuon", &nMuon);
+  t_old->SetBranchAddress("nJet", &nJet);
+  t_old->SetBranchAddress("Jet_btagDeepB", &Jet_btagDeepB);
+  t_old->SetBranchAddress("Muon_isGlobal", &Muon_isGlobal);
+
   TFile newfile("prueba1.root", "recreate");
   auto t_new = t_old->CloneTree(0);
 
-  UInt_t nMuon;
-  t_old->SetBranchAddress("nMuon", &nMuon);
-
   for (int i=0; i<nentries; i++){
     t_old->GetEntry(i);
+    bool passbTag = true;
+    for (int j=0; j<nJet; j++){
+      if (Jet_btagDeepB[j] > 0.5847){
+        passbTag = false;
+      };
+    };
+    bool passnMuon = false;
     if (nMuon == 4){
+      passnMuon = true;
+    };
+    bool passAllGlobal = true;
+    for (int k=0; k<nMuon; k++){
+      if (Muon_isGlobal[k] == 0){
+        passAllGlobal = false;
+      };
+    };
+    if (passAllGlobal && passbTag && passnMuon) {
       t_new->Fill();
     };
   };
